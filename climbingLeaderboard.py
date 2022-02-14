dc1fc1f2a106ffe22f0ea970f612211cabf806a6
KJTempas <kjtempas@gmail.com>
2022-02-14 14:14:42 -0600

    solved climbing for pretests; problems timing out w larger sets

--- climbingLeaderboard.py
+++ climbingLeaderboard.py
@@ -6,57 +6,25 @@
 # determine the new playe's rank after each score
 
 def climbingLeaderboard(ranked, player):
-    #loop through player scores
-    #add score to ranked and rerank
     res =[] #for rank each time new player score added
-    for p in player:
+    for p in player:     #loop through player scores
         #add score to ranked
         ranked.append(p)
-        #sort desc
-        ranked.sort(reverse=True)
-        print(ranked)
+        #change to set to remove dups
+        ranked_set = set(ranked)
+        #back to list, then sort
+        n_ranked = list(ranked_set)
+        new_ranked = sorted(n_ranked,reverse=True)
+        x = new_ranked.index(p)  # get index of p in new_ranked
+        res.append(x+1)  #so not zero based
 
-# def rank_players(ranked):
-    #find original ranks
-        new_ranks =[]
-        i = 1 #rank starts here
-        for r in range(len(ranked)-1): #loop through ranked scores
-           # print(ranked[r])
-            if ranked[r] > ranked[r+1]:
-                new_ranks.append(i)
-                #print('ranks1',ranks)
-                if ranked[r] == p:
-                    res.append(i)
-                i+=1
-                #how many are the same if equal? count?
-            elif ranked[r] == ranked[r+1]:
-                x = ranked.count(ranked[r])
-                for t in range(x):
-                    new_ranks.append(i) #append rank multiple times
-                    if ranked[r] == p:
-                        res.append(i)
-                    print('2',new_ranks)
-                i +=1
-                r +=x
-                #where is p in the ranks?
-        #need to deal with last num in ranked gets rank i+1
-        ranks.append(i+1)
-    print('res', res)
-
-#try sets
-#
-
-#tried scipy.stats - not appropriate for this type of ranking
- #y = [sorted(ranked).index(x) for x in ranked] #does not work either
-    
-    #add new score to ranked, then rerank each time
-    #make ranking a function  or use a while loop
-
+    return res
+ 
 #subtask - for 60% of max score: 1<=n<=200 and same for m
 
-climbingLeaderboard([100,90,90,80], [70,80,105]) # [4 3 1]
-#climbingLeaderboard([100,100,50,40,40,20,10], [5, 25,50,120]) #[6 4 2 1]
-#climbingLeaderboard([100,90,90,80,75,60],[50,65,77,90,102])# [6 5 4 2 1]
+print(climbingLeaderboard([100,90,90,80], [70,80,105])) # [4 3 1]
+print(climbingLeaderboard([100,100,50,40,40,20,10], [5, 25,50,120])) #[6 4 2 1]
+print(climbingLeaderboard([100,90,90,80,75,60],[50,65,77,90,102]))# [6 5 4 2 1]
 
 # if __name__ == '__main__':
 #     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -75,3 +43,34 @@ climbingLeaderboard([100,90,90,80], [70,80,105]) # [4 3 1]
 #     fptr.write('\n')
 
 #     fptr.close()
+
+
+
+#old method
+
+# # def rank_players(ranked):
+#     #find original ranks
+#         new_ranks =[]
+#         i = 1 #rank starts here
+#         for r in range(len(ranked)-1): #loop through ranked scores
+#            # print(ranked[r])
+#             if ranked[r] > ranked[r+1]:
+#                 new_ranks.append(i)
+#                 #print('ranks1',ranks)
+#                 if ranked[r] == p:
+#                     res.append(i)
+#                 i+=1
+#                 #how many are the same if equal? count?
+#             elif ranked[r] == ranked[r+1]:
+#                 x = ranked.count(ranked[r])
+#                 for t in range(x):
+#                     new_ranks.append(i) #append rank multiple times
+#                     if ranked[r] == p:
+#                         res.append(i)
+#                     print('2',new_ranks)
+#                 i +=1
+#                 r +=x
+#                 #where is p in the ranks?
+#         #need to deal with last num in ranked gets rank i+1
+#         ranks.append(i+1)
+#     print('res', res)
