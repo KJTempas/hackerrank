0401f575fcd0428499ddb8b31fd2b08efaa634bd
KJTempas <kjtempas@gmail.com>
2022-02-18 09:45:38 -0600

    finished capitalize

---
+++ capitalize.py
@@ -0,0 +1,22 @@
+def capitalize(s):
+    #split string at spaces if there are any
+    st = s.split(' ')
+    res = ''
+    for x in st:
+        res += x.capitalize() + ' '
+    return res
+
+# capitalize('chris alan') #Chris Alan
+# capitalize('12abc') #12abc
+# print(capitalize('12abc alan'))
+print(capitalize(' hello   world  lol'))  #extra spaces need to be retained
+
+#why does this not work ?
+# if __name__ == '__main__': 
+#     s = input()
+#     # import pdb
+#     # breakpoint()
+#     print(capitalize(s))
+
+
+
