808b63a3f7667cfc4a036a282493147e904c99f4
KJTempas <kjtempas@gmail.com>
2022-02-17 19:10:02 -0600

    finished runner_up

--- runner_up.py
+++ runner_up.py
@@ -1,9 +1,20 @@
 #given list of scores, find the runner-up score
 
-def runner_up(arr):
+def runner_up(arr): #completed 
     to_set = set(arr)  #to set to remove duplicates
     arr = list(to_set)     #back to list
     arr.remove(max(arr)) #find max and remove it from list
     return max(arr) #return new max (runner up)
 
-print(runner_up([2,3,6,6,5])) #5
+#print(runner_up([2,3,6,6,5])) #5
+
+
+if __name__ == '__main__':
+
+    n=int(input())
+    arr = map(int, input().split())
+
+    runner_up(arr)
+
+
+    #how do you get this to run from main?
