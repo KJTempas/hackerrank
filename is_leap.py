07767247f01c9434534e9efd8f5dd307f500cb89
KJTempas <kjtempas@gmail.com>
2022-02-19 21:19:03 -0600

    initial work on shoe_counter

--- is_leap.py
+++ is_leap.py
@@ -5,14 +5,9 @@
 #the year is also evenly divicible by 400.  Then it is a leap year
 
 def is_leap(year):
-    # if year % 4 == 0: #is a leap year unless
-    #     if year % 100 == 0: #is not a leap year unless
-    #         if year % 400 == 0: #is a leap year
-    #             print('True')
-    #     print('False')
     #TODO figure it out
-    if year % 400 == 0 and year % 100 ==0:
-        if year % 100 !=0 and year %400 == 0:
+    if year % 400 == 0 and (year % 100 !=0 and year %400 == 0):
+        
             print('True')
     else:
         print('False')
---
+++ shoe_counter.py
@@ -0,0 +1,26 @@
+#calculate amount of money earned by shoe store owner
+#input [list of shoe sizes', [(cust show size, cost), etc for a list of  customers]]
+#not sure about input of shoesize and cost;  or tuple?
+
+def shoe_counter(shoe_sizes):#, customers):
+    from collections import Counter
+    x = Counter(shoe_sizes)
+    print(x)
+    
+
+
+#shoe_counter([2,3,4,5,6,8,7,6,5,18])#, [(6,55), (6,45), (6,55) (4,40), (18, 60), (10,50)])
+
+if __name__ == '__main__':
+    #TODO figure out input
+    N = input() #number of shoe sizes
+    for 
+    sizes = []
+    sizes.append(x)
+   # make tuples size(index [0]) and cost[1]
+    transactions = []
+    size = input()
+    cost = input()
+    newtuple = tuple((size, cost))
+    transactions.append(newtuple)
+
