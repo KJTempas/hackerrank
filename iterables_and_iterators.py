6cdedcf5719c9daa87962bc796514a291dc07fc2
KJTempas <kjtempas@gmail.com>
2022-02-23 17:01:04 -0600

    solved iterables_and_iterators

--- iterables_and_iterators.py
+++ iterables_and_iterators.py
@@ -2,32 +2,40 @@
 #use 1 based indexing
 import itertools
 from itertools import combinations
-
+#Very tricky
 
 def iter(list1, k):
-    #get all combos of list1 w no repeats
-    # x =itertools.combinations(list1, k) #works
-    # print(list(x)) #returns  list of tuples
-    # print(type(list(x)))
-    # print(len(list(x))) #WHY???
-    
-    def is_valid(data):
+    def is_valid(data): #used in filtering list1 below
         if 'a' in data:
             return True
 
     x = filter(is_valid, itertools.combinations(list1, k))
-    print(x)  #this gives me 5 answers, which is correct
-    print(len(x)) #5, also correct
-    lenx = len(x)
-    print(lenx) #5
+    lenx = len(x) #this causes an Runtime Error in hackerrank
+    #where the message is TypeError: object of type 'filter' has no len()
+    #I had to sub this in hackerrank instead of line 13
+    # lenx=0
+    # for i in x:
+    #     lenx+= 1
+    #then it passed all tests
     #total # of combos from itertools.combinations
     y = itertools.combinations(list1, k)
-    print(len(list(y))) #6
-    leny = len(list(y)) #0?????
-    print(leny)
-    
+    def count_iterable(y): #can't do len on itertools combination object as is
+        return sum(1 for e in y)
+    leny = count_iterable(y)
 
-iter(['a','a','c','d'], 2)
+    return '{:.3f}'.format(float(lenx)/leny)
+
+#iter(['a','a','c','d'], 2) #0.8333
+
+
+if __name__== '__main__':
+    N = int(input()) # number of elements in list
+    def toString(a):
+        return str(a)
+    list1 = list(map(toString, input().split()))
+    k = int(input()) #number of indices to be selected
+
+    print(iter(list1, k))
 
 #helpful info
 #https://stackoverflow.com/questions/36211680/2n-itertools-combinations-with-advanced-filtering
