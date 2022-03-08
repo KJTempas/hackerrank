a33fccadf7a11701f5e4f8e3636448cc8604defa
KJTempas <kjtempas@gmail.com>
2022-03-08 16:10:47 -0600

    solved nested_lists

--- nested_lists.py
+++ nested_lists.py
@@ -5,63 +5,30 @@
 #https://betterprogramming.pub/10-important-tips-for-using-nested-lists-in-python-38ceca68be35
 
 
-#why does this not work in HackerRank?
 def nested_lists(records):
-    #convert nested list to dict
-    dict = {}
-    for sub_list in records:
-        dict[sub_list[0]] = sub_list[1] #dictname[key] = value
-    #remove min key value
-    min_value = min(dict.values())
-    for key,value in dict.items():
-        if value == min_value:
-            del dict[key]
-            
+    x = min(records, key=lambda x: x[1])  #the min value of each list, comparing the second element
+    #filter list, removing if value is == min
+    new_records = [ele for ele in records if ele[1]!=x[1]]
     #find min again
-    min_value =min(dict.values())
-
-    dict_items = dict.items()
-    sorted_items = sorted(dict_items) #sort alphabetically by key
-    for s in sorted_items: #loop through tuple
-        if s[1] == min_value:
-            print(s[0])
+    x = min(new_records, key=lambda x: x[1])
+    final_records = [ele for ele in new_records if ele[1]==x[1]]
+    #sort records by name, alphabetically
+    final_records.sort()
+    for record in final_records:
+        print(record[0])
 
 
 nested_lists([['chi', 20.0],['beta', 50.0], ['alpha', 50.0]]) #alpha beta
 nested_lists([['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]) # Berry Harry
 nested_lists([['Beria', 20], ['Varun', 19], ['Harsh', 20], ['Kakunami', 19], ['Vikas', 21]]) #Beria, Harsh
-#original in hackerrank
+# 
 # if __name__ == '__main__':
+#     records = []
 #     for _ in range(int(input())):
 #         name = input()
 #         score = float(input())
-
-
-
-#this almost works but fails 2/10 test cases
-# checkout one failed test case - it is line 32 above, which works on my code above
-# if __name__ == '__main__':
-#     records = []
-#     for _ in range(int(input())): #these input lines provided
-#         name = input()
-#         score = float(input())
-
 #         records.append([name, score])
+# nested_lists(records)
 
-#     dict = {}
-#     for sub_list in records:
-#         dict[sub_list[0]] = sub_list[1] #dictname[key] = value
-#     #remove min key value
-#     min_value = min(dict.values())
-#     for key,value in dict.items():
-#         if value == min_value:
-#             del dict[key]
-#             
-#     #find min again
-#     min_value =min(dict.values())
 
-#     dict_items = dict.items()
-#     sorted_items = sorted(dict_items) #sort alphabetically by key
-#     for s in sorted_items: #loop through tuple
-#         if s[1] == min_value: #if the value in the tuple is the min
-#             print(s[0])
+
