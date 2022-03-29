34cbde5d5f7c82c35b8976890e64e5ecb5d0a80e
KJTempas <kjtempas@gmail.com>
2022-03-28 19:22:59 -0500

    set mutations

--- set_diffsymmut.py
+++ set_diffsymmut.py
@@ -1,20 +1,20 @@
 #goal find total number of students wo have subscribed to only English newspaper
 #some get both) -so need total Engl - (Engl + French)
-def set_diff(A,B):
-    A = set(A)
-    B = set(B)
-    #print(A.difference(B)
-    print(A-B)
-    print(len(A-B))
+# def set_diff(A,B):
+#     A = set(A)
+#     B = set(B)
+#     #print(A.difference(B)
+#     print(A-B)
+#     print(len(A-B))
 
-def sym_diff(A,B):
-    #.symmetric_difference() operator returns a set w all the elemends that are
-    #in the set and the iterable but not both (not intersection)
-    #alt is a ^ operator but operatoes on the set of the elemens in set
-    A= set(A)
-    B = set(B)
-    print(A^B)
-    print(len(A^B))
+# def sym_diff(A,B):
+#     #.symmetric_difference() operator returns a set w all the elemends that are
+#     #in the set and the iterable but not both (not intersection)
+#     #alt is a ^ operator but operatoes on the set of the elemens in set
+#     A= set(A)
+#     B = set(B)
+#     print(A^B)
+#     print(len(A^B))
 
 
 #set_diff([1,2,3,4,5,6,7,8,9], [10,1,2,3,11,21,55,6,8]) #4
@@ -48,20 +48,16 @@ def sym_diff(A,B):
 #.difference_update() or -= means update set by removing elements found in an interable/another set
 #.symmetric_difference_update() or ^= means update set by only keeping the elements found in either set, but not in both
 
-def set_mut(A,B):
-    
+
+if __name__ == '__main__':
+    (_, A) = (int(input()),set(map(int, input().split())))
+    B = int(input())
+    for _ in range(B):
+        (command, newSet) = (input().split()[0],set(map(int, input().split())))
+        #getattr() returns the value of the named attribute of an object (or a default value)
+        #syntax is getattr(object, attribute, default)
+        getattr(A, command)(newSet)
+
+    print (sum(A))
 
 
-
-#test data for mutations - result should be 38
-#  16
-#  1 2 3 4 5 6 7 8 9 10 11 12 13 14 24 52
-#  4
-#  intersection_update 10
-#  2 3 5 6 8 9 1 4 7 11
-#  update 2
-#  55 66
-#  symmetric_difference_update 5
-#  22 7 35 62 58
-#  difference_update 7
-#  11 22 35 55 58 62 66
